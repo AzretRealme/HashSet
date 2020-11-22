021087e0d7d1d232de09c5634d3b6165b10b988e
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2020-11-22 18:59:42 +0600

    update
    update

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
         for(Integer w: linkedHashSet){
             System.out.println(w);
         }
-        System.out.println(linkedHashset.size());
+        System.out.println(linkedHashSet.size());
         System.out.println(linkedHashSet.isEmpty());
         System.out.println(linkedHashSet.contains(10));
         System.out.println(linkedHashSet.remove(12));
